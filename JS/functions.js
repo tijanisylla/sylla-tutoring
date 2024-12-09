69436398ce73c2946e22e7cc8ba53ce432fefc83
Tijani Sylla <tijani.sylla1@gmail.com>
2024-12-09 13:54:44 -0600

    added more files to exercise

---
+++ JS/functions.js
@@ -0,0 +1,40 @@
+// Standard funtion
+function myFunction() {
+  console.log("Hello World");
+}
+
+// Function with parameters
+function myFunction2(name, lastname) {
+  console.log("Hello " + name + " " + lastname);
+}
+
+// Function with return
+function myFunction3(a, b, c) {
+  return a * b * c;
+}
+
+// Function with default parameters
+function myFunction4(a, b = 3) {
+  return a * b;
+}
+
+// Function with rest parameters
+function myFunction5(a, ...rest) {
+  console.log(a);
+  console.log(rest);
+}
+
+// Type of functions
+
+// Function expression
+const myFunction6 = function () {};
+
+// Arrow function
+const myFunction7 = () => {
+  console.log("Hello World");
+};
+
+// Arrow function with parameters
+const myFunction8 = (name, lastname) => {
+  console.log("Hello " + name + " " + lastname);
+};
