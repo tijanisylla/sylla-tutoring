34211426beeed0e191f5c3c972be9b36929276fa
Tijani Sylla <tijani.sylla1@gmail.com>
2025-01-25 15:43:24 -0600

    Today Karina learned Modulus

---
+++ JS/Netflix.js
@@ -0,0 +1,42 @@
+// 4. netflixQueue is an object for managing your netflix queue
+const netflixQueue = {
+  queue: [
+    // Array of movies
+    "Mr. Nobody",
+    "The Matrix",
+    "Eternal Sunshine of the Spotless Mind",
+    "Fight Club",
+  ],
+  watchMovie() {
+    this.queue.pop(); // Watched it already
+    return "Watched a movie!";
+  },
+  addMovie(movie) {
+    // Adding a movie
+    this.queue.unshift(movie);
+    return "Adding a movie!";
+  },
+  printQueue() {
+    // Printing a movie
+    let list = "";
+    for (let i = this.queue.length - 1; i >= 0; i--) {
+      const currentMovie = this.queue[i];
+      list += `${this.queue.length - i}. ${currentMovie}\n`;
+    }
+    return list;
+  },
+};
+
+console.log(`
+  Printing movie queue!
+
+  ${netflixQueue.printQueue()}
+  ${netflixQueue.watchMovie()}
+  Printing movie queue!
+
+  ${netflixQueue.printQueue()}
+  ${netflixQueue.addMovie("Black Swan")}
+  Printing movie queue!
+
+  ${netflixQueue.printQueue()}
+  `);
---
+++ JS/modulus.js
@@ -0,0 +1,17 @@
+function oddNumber(number) {
+  if (number % 2 === 0) {
+    console.log(`${number} is Even`);
+  } else {
+    console.log(`${number} is Odd`);
+  }
+}
+
+// oddNumber(7); //  Odd
+// oddNumber(10); //  Even
+
+// ? Debuggin
+console.log(7 / 2); // 3.5
+console.log(7 % 2); // 1 === 0 Odd
+
+console.log(10 / 2); // 5 === 0
+console.log(10 % 2); // 0 === 0 EVEN
